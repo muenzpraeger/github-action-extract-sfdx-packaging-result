3063fe30909f0c4a502cb0397ee91e7fcf3a8633
muenzpraeger <mail@winkelmeyer.com>
2019-11-29 10:49:26 +0100

    Refine action

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ async function run() {
     try {
         const jsonResponse = JSON.parse(core.getInput('json'));
         core.info('Result is: ' + jsonResponse.status);
+        core.info(
+            'Package Version Id: ' +
+                jsonResponse.result.SubscriberPackageVersionId
+        );
         if (jsonResponse.status > 0) {
             core.setFailed(jsonResponse.result.ErrorMessages);
+        }
+        if (!jsonResponse.result.ErrorMessages) {
+            core.setFailed('No package version id found.');
         }
         core.setOutput('isSuccess', jsonResponse.status === 0 ? true : false);
         core.setOutput(
